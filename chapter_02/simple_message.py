e5bdd2bb1de8518ef77b0240594f1c4cb0694feb
setyanoegraha <pandu4688@gmail.com>
2025-03-24 23:06:11 +0700

    Update the contents of this directory

---
+++ chapter_02/simple_message.py
@@ -0,0 +1,2 @@
+simple_message = "You're a good boy! Stay Hard and Consistent Buddy."
+print(simple_message)
---
+++ chapter_02/simple_messages.py
@@ -0,0 +1,2 @@
+simple_messages = "Don't forget the life promised yourself. Instead of focusing on being the best, just focus on being slightly better each and every day."
+print(simple_messages)
