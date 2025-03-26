ee393c8938f9e01d44705610c02b480e4b172356
setyanoegraha <pandu4688@gmail.com>
2025-03-27 06:15:05 +0700

    feat: demonstrate how to work with lists

--- chapter_03/guided_exercises/bicycles.py
+++ chapter_03/guided_exercises/bicycles.py
@@ -1,5 +1,12 @@
+
 bicycles = ['trek', 'cannondale', 'redline', 'specialized']
 print(bicycles)
 
 # Accessing elements in a list
 print(bicycles[0])
+
+# Accessing elements in a list with formatting using title() method
+print(bicycles[2].title())
+
+# Accessing the elements last item in the list
+print(bicycles[-1].upper())
