5c8aa933461e23b6cb657456608d0597a2e571b4
setyanoegraha <pandu4688@gmail.com>
2025-04-02 07:13:47 +0700

    docs: add explanation for guest list management script

--- chapter_03/self_exercises/exe_3-5_changing_guest_list.py
+++ chapter_03/self_exercises/exe_3-5_changing_guest_list.py
@@ -30,9 +30,40 @@ print(
 
 # Removing the guest who can't attend in a mosque
 guests.remove(unable_to_attends)
+print("These are the guests who could attend:")
 print(guests)
 
 # Replace the guest who can't attend  with a new guest
 new_guest = 'einar'
 guests.insert(0, new_guest)
+print("\nThese are the new list guests who could attend:")
 print(guests)
+
+# Print personalized invitations message to the new guests list
+print(
+    f"\nGood Night, My Beloved Bestfriend {guests[0].title()}, Since we've been on friendly terms for a long time. I invite you all to dinner on March 28 at Avenue Street, Brooklyn Cafe.")
+print(
+    f"\nGood Night, My Beloved Bestfriend {guests[1].title()}, Since we've been on friendly terms for a long time. I invite you all to dinner on March 28 at Avenue Street, Brooklyn Cafe.")
+print(
+    f"\nGood Night, My Beloved Bestfriend {guests[2].title()}, Since we've been on friendly terms for a long time. I invite you all to dinner on March 28 at Avenue Street, Brooklyn Cafe.")
+print(
+    f"\nGood Night, My Beloved Bestfriend {guests[3].title()}, Since we've been on friendly terms for a long time. I invite you all to dinner on March 28 at Avenue Street, Brooklyn Cafe.")
+print(
+    f"\nGood Night, My Beloved Bestfriend {guests[4].title()}, Since we've been on friendly terms for a long time. I invite you all to dinner on March 28 at Avenue Street, Brooklyn Cafe.")
+print(
+    f"\nGood Night, My Beloved Bestfriend {guests[5].title()}, Since we've been on friendly terms for a long time. I invite you all to dinner on March 28 at Avenue Street, Brooklyn Cafe.")
+print(
+    f"\nGood Night, My Beloved Bestfriend {guests[6].title()}, Since we've been on friendly terms for a long time. I invite you all to dinner on March 28 at Avenue Street, Brooklyn Cafe.")
+print(
+    f"\nGood Night, My Beloved Bestfriend {guests[-1].title()}, Since we've been on friendly terms for a long time. I invite you all to dinner on March 28 at Avenue Street, Brooklyn Cafe.")
+
+"""
+Explanation:
+
+1️⃣ **Creating a guest list:** I store names of friends in a list called `guests`.
+2️⃣ **Printing invitations:** I use f-strings and `.title()` to format names properly while inviting each guests.
+3️⃣ **Handling an unavailable guest:**
+    - A variable `unable_to_attends` stores the name of a guest who can't attend.
+    - I remove this guest from the list using `.remove()`.
+    
+"""
