c5c795763453199a11832327ecc4d1661bd9ac80
setyanoegraha <pandu4688@gmail.com>
2025-04-02 22:22:58 +0700

    feat: added how to modify a list.

---
+++ chapter_03/guided_exercises/motorcycles.py
@@ -0,0 +1,13 @@
+# Modifying Elements in a List
+
+# Initialize a motorcycles list with three elements
+print("Modifying elements in a list:")
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles)
+
+# modify the first elements with 'ducati'
+motorcycles[0] = 'ducati'
+print(motorcycles)
+
+# Adding elements to a list
+#
